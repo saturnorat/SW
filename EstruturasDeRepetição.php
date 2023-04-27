be12764bce0855844e3d569940bef06e141eb559
SaturnoRat <130088047+saturnorat@users.noreply.github.com>
2023-04-26 23:11:32 -0300

    Create EstruturasDeRepetição.php

---
+++ EstruturasDeRepetição.php
@@ -0,0 +1,27 @@
+//Imprime números de 0 até 15
+<?php
+  for($cont = 0; $cont<=10; $cont++){
+    echo "Valor do contador: ".$cont."<br>";
+}
+echo "Fim da repetição";
+?>
+//////////////////////////////////////////////////////
+
+//Imprime números de 12 até 24
+<?php
+  for($cont = 12; $cont<=24; $cont++){
+    echo "Valor do contador: ".$cont."<br>";
+}
+echo "Fim da repetição";
+?>
+////////////////////////////////////////////////////
+
+//Imprime números ímpares de 7 a 31
+<?php
+  for ($i = 7; $i <= 31; $i++) {
+    if ($i % 2 != 0) {
+       echo $i . " ";
+  }
+}
+?>
+
